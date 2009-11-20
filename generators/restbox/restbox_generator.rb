d59b3d3014dac39d207633f878856f1850ffdef0
Richard Millan <richardiux@gmail.com>
2009-11-19 16:38:51 -0800

    adding generators for css, js, and images

---
+++ generators/restbox/restbox_generator.rb
@@ -0,0 +1,27 @@
+class RestboxGenerator < Rails::Generator::Base
+  
+  def initialize(*runtime_args)
+    super
+  end
+  
+  def manifest
+    record do |m|
+      m.directory File.join('public', 'stylesheets')
+      m.template 'stylesheets/restbox.css',   File.join('public', 'stylesheets', 'restbox.css')
+      
+      m.directory File.join('public', 'javascripts')
+      m.template 'javascripts/restbox.js',   File.join('public', 'javascripts', 'restbox.js')
+      
+      m.directory File.join('public', 'images')
+      m.template 'images/loader.gif',   File.join('public', 'images', 'loader.gif')
+      m.template 'images/restbox_close.png',   File.join('public', 'images', 'restbox_close.png')
+    end
+  end
+    
+  protected
+  
+  def banner
+    %{Usage: #{$0} #{spec.name}\nCopies restbox.css, restbox.js, and images to it's proper location to public}
+  end
+  
+end
--- install.rb
+++ install.rb
@@ -1,18 +1,1 @@
-require 'fileutils'
-
-public_path = File.dirname(__FILE__) + '/../../../public'
-images_path = public_path + '/images/restbox'
-
-close_img   = images_path + '/restbox_close.png'
-loading_img = images_path + '/loader.gif'
-restbox_js  = public_path + '/javascripts/restbox.js'
-restbox_css = public_path + '/stylesheets/restbox.css'
-
-FileUtils.makedirs images_path
-
-FileUtils.cp_r File.dirname(__FILE__) + '/public/images/restbox_close.png', close_img
-FileUtils.cp_r File.dirname(__FILE__) + '/public/images/loader.gif', loading_img
-FileUtils.cp_r File.dirname(__FILE__) + '/public/javascripts/restbox.js', restbox_js
-FileUtils.cp_r File.dirname(__FILE__) + '/public/stylesheets/restbox.css', restbox_css
-
-puts IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+puts "Run `./script/generate restbox` if you want (copies optional config file and some stylesheets into your app)"
